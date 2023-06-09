6f4a13a8c368272e6b5742f71333dcca039e0eb1
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2023-06-09 08:38:43 -0400

    changes

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,9 +2,9 @@
 
 ENV_FILE=./.env
 
-# chown -R www-data:www-data .
+chown -R www-data:www-data .
 
-# composer install
+composer install
 
 if [ -f "$ENV_FILE" ]; then
     echo "$ENV_FILE" exists;
@@ -14,6 +14,6 @@ fi
 
 php artisan key:generate
 
-php artisan migrate
+# php artisan migrate
 
 php-fpm
